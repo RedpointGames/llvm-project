e77d38876401d77f74124f6224bb963653bdbdc8
June Rhodes <jrhodes@redpoint.games>
2024-09-26 22:31:06 +1000

    Update build script to use UBA for CMake

--- .redpoint/build.ps1
+++ .redpoint/build.ps1
@@ -2,25 +2,60 @@ param([switch] $Generate, [switch] $Install, [switch] $InstallOnly, [switch] $De
 
 $global:ErrorActionPreference = 'Stop'
 
+function Invoke-CmdScript {
+  param(
+    [String] $scriptName
+  )
+  $cmdLine = """$scriptName"" $args & set"
+  & $Env:SystemRoot\system32\cmd.exe /c $cmdLine |
+  select-string '^([^=]*)=(.*)$' | foreach-object {
+    $varName = $_.Matches[0].Groups[1].Value
+    $varValue = $_.Matches[0].Groups[2].Value
+    set-item Env:$varName $varValue
+  }
+}
+
+$UbaViaUet = (Test-Path "C:\ProgramData\UET\Current\uet.exe")
+
 Push-Location "$PSScriptRoot\.."
 try {
     # Set the build path.
     $BuildPathDebug = "build\win64\debug"
     $BuildPathRelease = "build\win64\release"
+    $LauncherFlags = @()
+    $UbaCores = @()
+    if ($UbaViaUet) {
+        $LauncherFlags += "-DCMAKE_C_COMPILER_LAUNCHER=$PSScriptRoot\uet-cmake.bat"
+        $LauncherFlags += "-DCMAKE_CXX_COMPILER_LAUNCHER=$PSScriptRoot\uet-cmake.bat"
+        $UbaCores += "-j256"
+    }
+
+    # Create the session ID for this build.
+    $env:CMAKE_UBA_SESSION_ID = "$(Get-Random)"
+
+    # Initialize MSVC environment.
+    Invoke-CmdScript "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 
     # Generate CMake projects if needed.
     if ($Generate `
-        -or (!(Test-Path "$BuildPathDebug\LLVM.sln")) `
-        -or (!(Test-Path "$BuildPathRelease\LLVM.sln"))) {
+        -or (!(Test-Path "$BuildPathDebug\CMakeCache.txt")) `
+        -or (!(Test-Path "$BuildPathRelease\CMakeCache.txt"))) {
         if (!(Test-Path $BuildPathDebug)) {
             New-Item -ItemType Directory $BuildPathDebug | Out-Null
         }
         if (!(Test-Path $BuildPathRelease)) {
             New-Item -ItemType Directory $BuildPathRelease | Out-Null
         }
-        & 'C:\Program Files\CMake\bin\cmake.exe' `
-            -T host=x64 -A x64 `
+            #-T host=x64 -A x64 `
+        & 'C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe' `
+            -G "Ninja" `
+            "-DCMAKE_MAKE_PROGRAM=C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\Ninja\ninja.exe" `
             "-DLLVM_ENABLE_PROJECTS:STRING=clang;lld" `
+            "-DCMAKE_C_COMPILER=C:\Program Files\LLVM\bin\clang-cl.exe" `
+            "-DCMAKE_CXX_COMPILER=C:\Program Files\LLVM\bin\clang-cl.exe" `
+            "-DCMAKE_ASM_COMPILER=C:\Program Files\LLVM\bin\clang-cl.exe" `
+            "-DCMAKE_RC_COMPILER=C:/Program Files (x86)/Windows Kits/10/bin/10.0.22621.0/x64/rc.exe" `
+            "-DCMAKE_MT=C:/Program Files (x86)/Windows Kits/10/bin/10.0.22621.0/x64/mt.exe" `
             -DLLVM_INSTALL_TOOLCHAIN_ONLY:BOOL=TRUE `
             -DLLVM_INCLUDE_BENCHMARKS:BOOL=FALSE `
             -DLLVM_INCLUDE_DOCS:BOOL=FALSE `
@@ -30,15 +65,22 @@ try {
             -DCLANG_INCLUDE_DOCS:BOOL=FALSE `
             -DLLVM_ENABLE_DIA_SDK:BOOL=FALSE `
             -DCMAKE_BUILD_TYPE=Debug `
-            -DCMAKE_CONFIGURATION_TYPES=Debug `
+            -DCMAKE_CFG_INTDIR=Debug `
+            $LauncherFlags `
             -Hllvm `
             "-B$BuildPathDebug"
         if ($LastExitCode -ne 0) {
             exit $LastExitCode
         }
-        & 'C:\Program Files\CMake\bin\cmake.exe' `
-            -T host=x64 -A x64 `
+        & 'C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe' `
+            -G "Ninja" `
+            "-DCMAKE_MAKE_PROGRAM=C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\Ninja\ninja.exe" `
             "-DLLVM_ENABLE_PROJECTS:STRING=clang;lld" `
+            "-DCMAKE_C_COMPILER=C:\Program Files\LLVM\bin\clang-cl.exe" `
+            "-DCMAKE_CXX_COMPILER=C:\Program Files\LLVM\bin\clang-cl.exe" `
+            "-DCMAKE_ASM_COMPILER=C:\Program Files\LLVM\bin\clang-cl.exe" `
+            "-DCMAKE_RC_COMPILER=C:/Program Files (x86)/Windows Kits/10/bin/10.0.22621.0/x64/rc.exe" `
+            "-DCMAKE_MT=C:/Program Files (x86)/Windows Kits/10/bin/10.0.22621.0/x64/mt.exe" `
             -DLLVM_INSTALL_TOOLCHAIN_ONLY:BOOL=TRUE `
             -DLLVM_INCLUDE_BENCHMARKS:BOOL=FALSE `
             -DLLVM_INCLUDE_DOCS:BOOL=FALSE `
@@ -48,7 +90,8 @@ try {
             -DCLANG_INCLUDE_DOCS:BOOL=FALSE `
             -DLLVM_ENABLE_DIA_SDK:BOOL=FALSE `
             -DCMAKE_BUILD_TYPE=Release `
-            -DCMAKE_CONFIGURATION_TYPES=Release `
+            -DCMAKE_CFG_INTDIR=Release `
+            $LauncherFlags `
             -Hllvm `
             "-B$BuildPathRelease"
         if ($LastExitCode -ne 0) {
@@ -66,9 +109,15 @@ try {
 
     # Build if not only installing.
     if (!$InstallOnly) {
-        & 'C:\Program Files\CMake\bin\cmake.exe' `
+        # Start UBA worker if needed.
+        if ($UbaViaUet) {
+            Start-Process -NoNewWindow -FilePath "C:\Work\uet\UET\uet\bin\Debug\net8.0\win-x64\uet.exe" -ArgumentList @("internal", "cmake-uba-server")
+        }
+
+        & 'C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe' `
             --build $BuildPath `
-            --config $BuildConfiguration
+            --config $BuildConfiguration `
+            $UbaCores
         if ($LastExitCode -ne 0) {
             exit $LastExitCode
         }
@@ -76,7 +125,7 @@ try {
 
     # Install if requested.
     if ($Install -or $InstallOnly) {
-        & 'C:\Program Files\CMake\bin\cmake.exe' `
+        & 'C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe' `
             --install $BuildPath `
             --config $BuildConfiguration
         if ($LastExitCode -ne 0) {
---
+++ .redpoint/uet-cmake.bat
@@ -0,0 +1,2 @@
+@echo off
+C:\Work\uet\UET\uet\bin\Debug\net8.0\win-x64\uet.exe internal cmake-uba-run -- %*
