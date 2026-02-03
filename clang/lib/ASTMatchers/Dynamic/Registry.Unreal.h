7541a23ea5764652f03aa88974e8258a89c06bb1
June Rhodes <jrhodes@redpoint.games>
2026-02-03 16:21:45 +1100

    Add 'hasRedundantNamespacing' AST matcher to find type specifiers with unnecessary namespace components

--- clang/lib/ASTMatchers/Dynamic/Registry.Unreal.h
+++ clang/lib/ASTMatchers/Dynamic/Registry.Unreal.h
@@ -4,4 +4,5 @@ REGISTER_MATCHER(forNone);
 REGISTER_MATCHER(forNoDescendant);
 REGISTER_MATCHER(isPODType);
 REGISTER_MATCHER(isMissingDllImportOrExport);
+REGISTER_MATCHER(hasRedundantNamespacing);
 // @unreal: END
