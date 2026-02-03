643950cfba4aa1548425c806e0e35f6d41d7758b
June Rhodes <jrhodes@redpoint.games>
2026-02-03 16:23:37 +1100

    Add AST matchers for UHT metadata

--- clang/lib/ASTMatchers/Dynamic/Registry.Unreal.h
+++ clang/lib/ASTMatchers/Dynamic/Registry.Unreal.h
@@ -5,4 +5,16 @@ REGISTER_MATCHER(forNoDescendant);
 REGISTER_MATCHER(isPODType);
 REGISTER_MATCHER(isMissingDllImportOrExport);
 REGISTER_MATCHER(hasRedundantNamespacing);
+REGISTER_MATCHER(isUClass);
+REGISTER_MATCHER(isUStruct);
+REGISTER_MATCHER(isUInterface);
+REGISTER_MATCHER(isIInterface);
+REGISTER_MATCHER(isUFunction);
+REGISTER_MATCHER(isUProperty);
+REGISTER_MATCHER(hasUSpecifier);
+REGISTER_MATCHER(hasUSpecifierValue);
+REGISTER_MATCHER(hasUMetadata);
+REGISTER_MATCHER(hasUMetadataValue);
+REGISTER_MATCHER(withIInterface);
+REGISTER_MATCHER(withUInterface);
 // @unreal: END
